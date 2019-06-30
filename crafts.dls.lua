f3c872428794a2da95d0493948a62647fdacf84f
Denr01 <denr01@users.noreply.github.com>
2019-06-30 23:49:42 +0300

    Create crafts.dls.lua

---
+++ crafts.dls.lua
@@ -0,0 +1,23 @@
+a = 3 + 4 * 2 / ((1 - 5)*2) + 5;
+
+while a > 1 do
+    print(a);
+    print(" ");
+    a = a - 1;
+end;
+
+println("");
+
+fact = 6;
+n = fact;
+while n > 1 do
+    n = n - 1;
+    fact = fact * n;
+end;
+print("Fact of " + fact);
+
+print("Fact: ");
+println(fact);
+println(3 * 2^10);
+println(3 / 2);
+
